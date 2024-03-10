420b1ebabeb75fef08348a4557012cc3be7ce131
Dlprogrammmer <1656892276@qq.com>
2024-03-10 22:53:44 +0800

    1.4

--- User/main.c
+++ User/main.c
@@ -46,4 +46,135 @@ int main(void)
 			OneNet_RevPro(dataPtr);
 		delay_ms(10);
 	}
+	unsigned short timeCount = 0;	//发送间隔变量
+	unsigned char *dataPtr = NULL;
+	static u8 lineNow;
+	Usart1_Init(115200);//debug串口
+		DEBUG_LOG("\r\n");
+		DEBUG_LOG("UART1初始化			[OK]");
+	delay_init();	    	 //延时函数初始化	
+		DEBUG_LOG("延时函数初始化			[OK]");
+	 
+	 delay_ms(500);// 延迟一下等待oled识别
+	OLED_Init();
+	OLED_ColorTurn(0);//0正常显示，1 反色显示
+  OLED_DisplayTurn(0);//0正常显示 1 屏幕翻转显示
+	OLED_Clear();
+	 	DEBUG_LOG("OLED1初始化			[OK]");
+		OLED_Refresh_Line("OLED");
+
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);// 设置中断优先级分组2
+		DEBUG_LOG("中断优先初始化			[OK]");
+		OLED_Refresh_Line("NVIC");
+	LED_Init();		  	//初始化与LED连接的硬件接口
+		DEBUG_LOG("LED初始化			[OK]");
+		OLED_Refresh_Line("Led");
+	KEY_Init();          	//初始化与按键连接的硬件接口
+		DEBUG_LOG("按键初始化			[OK]");
+		OLED_Refresh_Line("Key");
+	EXTIX_Init();		//外部中断初始化
+		DEBUG_LOG("外部中断初始化			[OK]");
+		OLED_Refresh_Line("EXIT");
+	BEEP_Init();
+		DEBUG_LOG("蜂鸣器初始化			[OK]");
+		OLED_Refresh_Line("Beep");
+	DHT11_Init();
+		DEBUG_LOG("DHT11初始化			[OK]");
+		OLED_Refresh_Line("DHT11");
+  BH1750_Init();
+		DEBUG_LOG("BH1750初始化			[OK]");
+		OLED_Refresh_Line("BH1750");
+	Usart2_Init(115200);//stm32-8266通讯串口
+		DEBUG_LOG("UART2初始化			[OK]");
+		OLED_Refresh_Line("Uart2");
+	
+		DEBUG_LOG("硬件初始化			[OK]");
+		
+	delay_ms(1000);
+	
+	DEBUG_LOG("初始化ESP8266 WIFI模块...");
+	if(!ESP8266_INIT_OK){
+		OLED_Clear();
+		OLED_ShowString(0,0,(u8*)"WiFi",16,1);
+		OLED_ShowChinese(32,0,8,16,1);//连
+		OLED_ShowChinese(48,0,9,16,1);//接
+		OLED_ShowChinese(64,0,10,16,1);//中
+		OLED_ShowString(80,0,(u8*)"...",16,1);
+		
+		OLED_Refresh();
+	}
+	ESP8266_Init();					//初始化ESP8266
+	
+	OLED_Clear();
+		OLED_ShowChinese(0,0,4,16,1);//服
+		OLED_ShowChinese(16,0,5,16,1);//务
+		OLED_ShowChinese(32,0,6,16,1);//器
+		OLED_ShowChinese(48,0,8,16,1);//连
+		OLED_ShowChinese(64,0,9,16,1);//接
+		OLED_ShowChinese(80,0,10,16,1);//中
+		OLED_ShowString(96,0,(u8*)"...",16,1);
+	OLED_Refresh();	
+	
+	while(OneNet_DevLink()){//接入OneNET
+		delay_ms(500);
+	}		
+	
+	OLED_Clear();	
+	
+	TIM2_Int_Init(4999,7199);
+	TIM3_Int_Init(2499,7199);
+	
+	BEEP = 0;//鸣叫提示接入成功
+	delay_ms(250);
+	BEEP = 1;
+	
+	OneNet_Subscribe(devSubTopic, 1);
+	
+	while(1)
+	{
+		if(timeCount % 40 == 0)//1000ms / 25 = 40 一秒执行一次
+		{
+			/********** 温湿度传感器获取数据**************/
+			DHT11_Read_Data(&humidityH,&humidityL,&temperatureH,&temperatureL);
+			
+			/********** 光照度传感器获取数据**************/
+			if (!i2c_CheckDevice(BH1750_Addr))
+			{
+				Light = LIght_Intensity();
+			}
+			
+			/********** 读取LED0的状态 **************/
+			Led_Status = GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_4);
+			
+			/********** 报警逻辑 **************/
+			if(alarm_is_free == 10)//报警器控制权是否空闲 alarm_is_free == 10 初始值为10
+			{
+				if((humidityH < 80) && (temperatureH < 30) && (Light < 1000))alarmFlag = 0;
+				else alarmFlag = 1;
+			}
+			if(alarm_is_free < 10)alarm_is_free++;
+//			DEBUG_LOG("alarm_is_free = %d", alarm_is_free);
+//			DEBUG_LOG("alarmFlag = %d\r\n", alarmFlag);
+
+			
+			/********** 输出调试信息 **************/
+			DEBUG_LOG(" | 湿度：%d.%d C | 温度：%d.%d %% | 光照度：%.1f lx | 指示灯：%s | 报警器：%s | ",humidityH,humidityL,temperatureH,temperatureL,Light,Led_Status?"关闭":"【启动】",alarmFlag?"【启动】":"停止");
+		}
+		if(++timeCount >= 200)	// 5000ms / 25 = 200 发送间隔5000ms
+		{
+			Led_Status = GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_4);//读取LED0的状态
+			DEBUG_LOG("==================================================================================");
+			DEBUG_LOG("发布数据 ----- OneNet_Publish");
+			sprintf(PUB_BUF,"{\"Hum\":%d.%d,\"Temp\":%d.%d,\"Light\":%.1f,\"Led\":%d,\"Beep\":%d}",
+				humidityH,humidityL,temperatureH,temperatureL,Light,Led_Status?0:1,alarmFlag);
+			OneNet_Publish(devPubTopic, PUB_BUF);
+			DEBUG_LOG("==================================================================================");
+			timeCount = 0;
+			ESP8266_Clear();
+		}
+		
+		dataPtr = ESP8266_GetIPD(3);
+		if(dataPtr != NULL)
+			OneNet_RevPro(dataPtr);
+		delay_ms(10);
 }
--- mqttkit.d
+++
@@ -1,4 +0,0 @@
-./mqttkit.o: Net\MqttKit.c Net\MqttKit.h Net\Common.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdlib.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h
